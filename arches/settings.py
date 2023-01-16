ad981ccc3a95c3e7fd29079ac19d02b2dae895a6
Chantelle <chantelle.smith@HistoricEngland.org.uk>
2023-01-16 17:05:05 +0000

    Add Tile Excel Export to settings.py

--- arches/settings.py
+++ arches/settings.py
@@ -135,10 +135,14 @@ ARCHES_NAMESPACE_FOR_DATA_EXPORT = "http://localhost:8000/"
 # ordered as seen in the resource cards or not.
 EXPORT_DATA_FIELDS_IN_CARD_ORDER = False
 
-# This is used to indicate whether temporary export files for Tile Excel Export should
-# be deleted or not
+# The TILE_EXCEL_EXPORT_TEMP_DIRECTORY and TILE_EXCEL_EXPORT_TEMP_FILE_DELETE settings
+# are only required by Windows instances of an Arches installation to ensure a successful
+# generation of data export in tile excel format.
+# The files generated in TILE_EXCEL_EXPORT_TEMP_DIRECTORY when TILE_EXCEL_EXPORT_TEMP_FILE_DELETE
+# is set to False will need to be cleared out periodically.
 
-DELETE_TEMP_EXCEL_EXPORT_FILE = True
+TILE_EXCEL_EXPORT_TEMP_DIRECTORY = None
+TILE_EXCEL_EXPORT_TEMP_FILE_DELETE = True
 
 RDM_JSONLD_CONTEXT = {"arches": ARCHES_NAMESPACE_FOR_DATA_EXPORT}
 
